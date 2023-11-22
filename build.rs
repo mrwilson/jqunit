7f9a5c9dc36a3fea0c406fdc31a004b5a605c576
Alex Wilson <gpg@probablyfine.co.uk>
2023-11-22 18:06:18 +0000

    build: Add link path if on MacOS

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("cargo:rustc-link-lib=jq")
+    println!("cargo:rustc-link-lib=jq");
+    if let Some(_) = option_env!("DYLD_FALLBACK_LIBRARY_PATH") {
+        println!("cargo:rustc-link-search=/opt/homebrew/lib")
+    }
 }
