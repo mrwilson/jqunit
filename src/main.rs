8bd3f262dc9b23620a48843b7bcf7dd385f8c500
Alex Wilson <gpg@probablyfine.co.uk>
2023-11-22 18:06:17 +0000

    refactor(runner): Apply fmt, add helper for str -> jv_string

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ mod runner;
 use std::ffi::CStr;
 use std::ffi::CString;
 
-use crate::jq::jq::{jq_compile, jq_init, jq_next, jq_start, jq_teardown, jv_string, jv_string_value};
+use crate::jq::jq::{
+    jq_compile, jq_init, jq_next, jq_start, jq_teardown, jv_string, jv_string_value,
+};
 
 fn main() {
-
     unsafe {
-
         let mut jq = jq_init();
 
         let hello_world_string = jv_string(CString::new("Hello, world!").expect("whee").as_ptr());
@@ -19,8 +19,12 @@ fn main() {
         jq_compile(jq, CString::new(".").expect("whee").as_ptr());
         jq_start(jq, hello_world_string, 0);
 
-        println!("{}", CStr::from_ptr(jv_string_value(jq_next(jq))).to_str().expect("whee"));
+        println!(
+            "{}",
+            CStr::from_ptr(jv_string_value(jq_next(jq)))
+                .to_str()
+                .expect("whee")
+        );
         jq_teardown(&mut jq);
     }
-
 }
--- src/runner.rs
+++ src/runner.rs
@@ -1,31 +1,46 @@
 mod jqunit {
+    use crate::jq::jq::{
+        jq_compile, jq_get_lib_dirs, jq_init, jq_next, jq_set_attr, jq_start, jq_state, jv,
+        jv_array, jv_array_append, jv_array_get, jv_array_length, jv_copy, jv_dump_string,
+        jv_get_kind, jv_kind_JV_KIND_NULL, jv_null, jv_string, jv_string_value,
+    };
     use std::ffi::{CStr, CString};
-    use crate::jq::jq::{jq_compile, jq_get_lib_dirs, jq_init, jq_next, jq_set_attr, jq_start, jq_state, jv, jv_array, jv_array_append, jv_array_get, jv_array_length, jv_copy, jv_dump_string, jv_get_kind, jv_kind_JV_KIND_NULL, jv_null, jv_string, jv_string_value};
 
     pub struct Runner {
-        state: *mut jq_state
+        state: *mut jq_state,
     }
 
     pub fn jv_to_string(jv: jv) -> String {
         unsafe {
-            String::from(CStr::from_ptr(jv_string_value(jv_dump_string(jv, 0))).to_str().expect("a"))
+            String::from(
+                CStr::from_ptr(jv_string_value(jv_dump_string(jv, 0)))
+                    .to_str()
+                    .expect("a"),
+            )
+        }
+    }
+
+    pub fn jv_from_string(input: &str) -> jv {
+        unsafe {
+            return jv_string(
+                CString::new(input)
+                    .expect("Invalid value passed to jv_string")
+                    .as_ptr(),
+            );
         }
     }
 
     impl Runner {
         pub fn start() -> Runner {
             Runner {
-                state: unsafe { jq_init() }
+                state: unsafe { jq_init() },
             }
         }
 
         pub fn add_library(&self, path: &str) {
             unsafe {
-                let libs = jv_array_append(
-                    jq_get_lib_dirs(self.state),
-                    jv_string(CString::new(path).expect("path").as_ptr())
-                );
-                jq_set_attr(self.state, jv_string(CString::new("JQ_LIBRARY_PATH").expect("path").as_ptr()), libs);
+                let libs = jv_array_append(jq_get_lib_dirs(self.state), jv_from_string(path));
+                jq_set_attr(self.state, jv_from_string("JQ_LIBRARY_PATH"), libs);
             }
         }
 
@@ -43,15 +58,14 @@ mod jqunit {
                     .and_then(|cstr| cstr.to_str().ok())
                     .map(String::from)
             }
-
         }
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::fs;
     use crate::runner::jqunit::Runner;
+    use std::fs;
 
     #[test]
     fn should_execute_code_with_no_input() {
@@ -64,12 +78,17 @@ mod test {
     #[test]
     fn should_load_library_and_execute_code() {
         let runner = Runner::start();
-        runner.add_library(fs::canonicalize("./fixtures").expect("path exists").as_path().to_str().expect("path"));
+        runner.add_library(
+            fs::canonicalize("./fixtures")
+                .expect("path exists")
+                .as_path()
+                .to_str()
+                .expect("path"),
+        );
 
         assert_eq!(
             runner.execute_code_with_no_input("include \"simple_function\"; simple_function"),
             Some(String::from("2"))
         );
     }
-
 }
